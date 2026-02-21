a48f07e83604ba7da904ebac2e6e11d0a5e0368b
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-02-22 08:43:30 +0900

    create EC2

--- main.tf
+++ main.tf
@@ -6,4 +6,15 @@ provider "aws" {
 resource "aws_instance" "hello-world" {
   ami = "ami-088103e734f7e0529"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "first instance"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+dnf install -y nginx
+systemctl start nginx
+EOF
+  user_data_replace_on_change = true
 }
