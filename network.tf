36f330178bef38c05ac944e7e4ba72c2881e63f2
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-02-23 11:48:35 +0900

    Create subnet

--- network.tf
+++ network.tf
@@ -16,3 +16,62 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+# ------------------------
+# Subnet
+# ------------------------
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-public-subnet-1a"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-public-subnet-1c"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-private-subnet-1a"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-private-subnet-1c"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "private"
+  }
+}
